50b9e123c82d25d563e55f806c14085bf994510f
cody-y13 <yas13131@protonmail.com>
2020-12-22 15:43:32 +0530

    5-styled componentsgit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+//global style
+import Globalstyle from './components/globalstyle';
 //import pages
 import Aboutus from './pages/aboutus';
 
 function App() {
   return (
     <div className="App">
+      <Globalstyle />
       <Aboutus />
     </div>
   );
