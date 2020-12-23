86f96dac17f03f6db9c6587432921a6a3f173137
cody-y13 <yas13131@protonmail.com>
2020-12-23 10:02:32 +0530

    9. nav pages and styling them

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Globalstyle from './components/globalstyle';
 //import pages
 import Aboutus from './pages/aboutus';
 import Nav from './components/nav';
+import Contactus from './pages/contactus';
+import Ourwork from './pages/ourwork';
+//router
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Globalstyle />
       <Nav />
-      <Aboutus />
+      <Switch>
+        <Route path="/" exact>
+          <Aboutus />
+        </Route>
+        <Route path="/work">
+          <Ourwork />
+        </Route>
+        <Route path="/contact">
+          <Contactus />
+        </Route>
+      </Switch>
     </div>
   );
 }
